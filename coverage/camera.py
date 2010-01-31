17cec19ab10353ed5a5affe4935bb721542725bc
Aaron Mavrinac <mavrinac@gmail.com>
2010-01-31 16:24:39 -0500

    Completed (but untested) camera model with mu.

--- coverage/camera.py
+++ coverage/camera.py
@@ -7,10 +7,10 @@ Single-camera coverage model module.
 @license: GPL-3
 """
 
-from math import sqrt, sin, atan
-from fuzz import TrapezoidalFuzzyNumber, RealRange
+from math import sqrt, sin, atan, pi
+from fuzz import TrapezoidalFuzzyNumber
 
-from geometry import Pose
+from geometry import Angle, Point, Pose
 
 
 class Camera( object ):
@@ -56,34 +56,64 @@ class Camera( object ):
         # fuzzy sets for visibility
         ahl = atan( ( ou * su ) / ( 2. * f ) )
         ahr = atan( ( ( w - ou ) * su ) / ( 2. * f ) )
-        Cvh = TrapezoidalFuzzyNumber( \
-              RealRange( ( -sin( ahl ) + gamma, sin( ahr ) - gamma ) ),
-              RealRange( ( -sin( ahl ), sin( ahr ) ) ) )
-
+        self.Cvh = TrapezoidalFuzzyNumber( \
+                   ( -sin( ahl ) + gamma, sin( ahr ) - gamma ),
+                   ( -sin( ahl ), sin( ahr ) ) )
         avl = atan( ( ov * sv ) / ( 2. * f ) )
         avr = atan( ( ( h - ov ) * sv ) / ( 2. * f ) )
-        Cvv = TrapezoidalFuzzyNumber( \
-              RealRange( ( -sin( avl ) + gamma, sin( avr ) - gamma ) ),
-              RealRange( ( -sin( avl ), sin( avr ) ) ) )
+        self.Cvv = TrapezoidalFuzzyNumber( \
+                   ( -sin( avl ) + gamma, sin( avr ) - gamma ),
+                   ( -sin( avl ), sin( avr ) ) )
 
         # fuzzy set for resolution
         mr = min( ( w / ( 2. * sin( ( ahl + ahr ) / 2. ) ) ), 
                   ( h / ( 2. * sin( ( avl + avr ) / 2. ) ) ) )
         zr1 = ( 1. / r1 ) * mr
         zr2 = ( 1. / r2 ) * mr
-        Cr = TrapezoidalFuzzyNumber( RealRange( ( 0, zr1 ) ),
-                                     RealRange( ( 0, zr2 ) ) )
+        self.Cr = TrapezoidalFuzzyNumber( ( 0, zr1 ), ( 0, zr2 ) )
 
         # fuzzy set for focus
         zl = ( A * f * zS ) / ( A * f + min( su, sv ) * ( zS - f ) )
         zr = ( A * f * zS ) / ( A * f - min( su, sv ) * ( zS - f ) )
         zn = ( A * f * zS ) / ( A * f + cmax * ( zS - f ) )
         zf = ( A * f * zS ) / ( A * f - cmax * ( zS - f ) )
-        Cf = TrapezoidalFuzzyNumber( RealRange( ( zl, zr ) ),
-                                     RealRange( ( zn, zf ) ) )
+        self.Cf = TrapezoidalFuzzyNumber( ( zl, zr ) , ( zn, zf ) )
         
         # fuzzy set for direction
-        # TODO: deal with angle stuff (2pi -> 0...)
+        self.Cd = TrapezoidalFuzzyNumber( \
+                  ( Angle( pi / 2. + delta ), Angle( 3 * pi / 2. - delta ) )
+                  ( Angle( pi / 2. ), Angle( 3 * pi / 2. ) ) )
 
         # pose
         self.pose = pose
+
+    def mu( self, point, direction ):
+        """\
+        Return the membership degree (coverage) for a directional point.
+    
+        @param point: The point location.
+        @type point: L{geometry.Point}
+        @param direction: The direction of the point.
+        @type direction: C{tuple} of L{geometry.Angle}
+        @return: The membership degree (coverage) of the point.
+        @rtype: C{float}
+        """
+        campoint = ( -self.pose ).map( point )
+
+        # visibility
+        mu_v = min( self.Cvh.mu( campoint.x / campoint.z ),
+                    self.Cvv.mu( campoint.y / campoint.z ) )
+
+        # resolution
+        mu_r = self.Cr.mu( campoint.z )
+
+        # focus
+        mu_f = self.Cf.mu( campoint.z )
+
+        # direction
+        dirpose = Pose( Point( 0, 0, 0 ), rotation_matrix( direction ) )
+        dirangle = ( point - self.pose.T ).normal.angle( dirpose.map_rotate( Point( 0, 0, 1 ) ) )
+        mu_d = self.Cd.mu( dirangle )
+       
+        # return min( mu_v, mu_r, mu_f, mu_d )
+        return mu_v * mu_r * mu_f * mu_d
