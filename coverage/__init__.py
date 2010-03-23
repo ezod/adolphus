4048a10f838332a397116f906dac731b7bfb7057
Aaron Mavrinac <mavrinac@gmail.com>
2010-03-23 14:49:02 -0400

    Add init wrapper for whole coverage module.

---
+++ coverage/__init__.py
@@ -0,0 +1,15 @@
+"""\
+Coverage module.
+
+@author: Aaron Mavrinac
+@organization: University of Windsor
+@contact: mavrin1@uwindsor.ca
+@license: GPL-3
+"""
+
+__version__ = ( 0, 0, 0 )
+
+__all__ = [ 'geometry', 'camera' ]
+
+from geometry import *
+from camera import *
