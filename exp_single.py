fe9f89cd818e18c08e711aa19a275f2835616d63
Aaron Mavrinac <mavrinac@gmail.com>
2010-04-09 16:17:00 -0400

    Add experiment with real parameters for a single camera.

---
+++ exp_single.py
@@ -0,0 +1,15 @@
+from math import pi
+from fuzz import RealRange
+
+from coverage import Scene, Camera, MultiCameraSimple, Point
+
+print "Creating camera model..."
+C = Camera( 'C', 4.3214, 12.1, 0.00465, 0.00465, 684.7, 535.24, 1024, 1360, 784.2, 0, 3.0, 0.5, 0.036, 1.5 )
+print "Creating scene..."
+S = Scene( RealRange( ( -1000.0, 1000.0 ) ), RealRange( ( -1000.0, 1000.0 ) ), RealRange( ( -100.0, 2000.0 ) ), 100.0, pi / 4.0 )
+print "Creating discrete multi-camera model..."
+M = MultiCameraSimple( S, set( [ C ] ) )
+print "Updating in-scene model..."
+M._update()
+print "Visualizing..."
+M.visualize( scale = 10.0 )
