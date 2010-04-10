c84a5ff5660b42ba26ed8e26cbd3be44c340b090
Aaron Mavrinac <mavrinac@gmail.com>
2010-04-10 12:56:05 -0400

    Various experimentation.

--- exp_single.py
+++ exp_single.py
@@ -1,15 +1,23 @@
 from math import pi
 from fuzz import RealRange
+from numpy import arange
 
-from coverage import Scene, Camera, MultiCameraSimple, Point
+from coverage import Scene, Camera, MultiCameraSimple, Point, visual_axes
 
 print "Creating camera model..."
-C = Camera( 'C', 4.3214, 12.1, 0.00465, 0.00465, 684.7, 535.24, 1024, 1360, 784.2, 0, 3.0, 0.5, 0.036, 1.5 )
+C = Camera( 'C', 4.3214, 12.1, 0.00465, 0.00465, 680, 512, 1024, 1360, 784.2, 0, 3.0, 0.5, 0.036, 1.5 )
 print "Creating scene..."
-S = Scene( RealRange( ( -1000.0, 1000.0 ) ), RealRange( ( -1000.0, 1000.0 ) ), RealRange( ( -100.0, 2000.0 ) ), 100.0, pi / 4.0 )
+S = Scene( RealRange( ( -1000.0, 1000.0 ) ), RealRange( ( -1000.0, 1000.0 ) ), RealRange( ( 100.0, 2000.0 ) ), 100.0, pi / 2.0 )
+#for x in arange( -400, 500, 100 ):
+#    for y in arange( -400, 500, 100 ):
+#        for z in arange( 1100, 1300, 100 ):
+#            S.make_opaque( Point( x, y, z ) )
+#for p in S.opaque:
+#    p.visualize( scale = 50.0 )
 print "Creating discrete multi-camera model..."
 M = MultiCameraSimple( S, set( [ C ] ) )
 print "Updating in-scene model..."
-M._update()
+M.update()
 print "Visualizing..."
 M.visualize( scale = 40.0 )
+visual_axes( scale = 40.0 )
--- test_dirmu.py
+++ test_dirmu.py
@@ -28,9 +28,8 @@ def dir_mu( p, zeta ):
 
 if __name__ == "__main__":
     visual_axes( 1 )
-    for p in generate_points( Point( 5, 0, 5 ), pi / 4.0 ):
-        p.visualize( color = color.red, opacity = dir_mu( p, 1.5 ) )
-    for p in generate_points( Point( 0, 5, 5 ), pi / 4.0 ):
-        p.visualize( color = color.red, opacity = dir_mu( p, 1.5 ) )
-    for p in generate_points( Point( 5, 5, 5 ), pi / 4.0 ):
-        p.visualize( color = color.red, opacity = dir_mu( p, 1.5 ) )
+    for x in arange( -5, 10, 5 ):
+        for y in arange( -5, 10, 5 ):
+            for z in arange( 5, 15, 5 ):
+                for p in generate_points( Point( x, y, z ), pi / 2.0 ):
+                    p.visualize( color = color.red, opacity = dir_mu( p, 1.5 ) )
