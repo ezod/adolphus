fef9224ba761283ec74749ff43b01d04cde23ecc
Aaron Mavrinac <mavrinac@gmail.com>
2010-04-12 10:12:28 -0400

    Add D model, make opaque setting based on ranges, refactor point generation.

--- coverage/camera.py
+++ coverage/camera.py
@@ -13,7 +13,7 @@ from itertools import combinations
 from fuzz import IndexedSet, RealRange, TrapezoidalFuzzyNumber, FuzzySet, \
                  FuzzyElement
 
-from geometry import Angle, Point, DirectionalPoint, Pose, rotation_matrix
+from coverage.geometry import Point, DirectionalPoint, Pose
 
 try:
     import visual
@@ -47,36 +47,87 @@ class Scene( object ):
         self.pstep = pstep
         self.dstep = dstep
         self.opaque = set()
+        self.D = FuzzySet()
 
-    def make_opaque( self, p ):
+    def make_opaque( self, x, y, z ):
         """\
-        Add a point to the set of opaque voxels.
+        Add a range to the set of opaque voxels.
 
-        @param p: Discrete point to make opaque.
-        @type p: L{geometry.Point}
+        @param x: Range in the x direction.
+        @type x: L{fuzz.RealRange}
+        @param y: Range in the y direction.
+        @type y: L{fuzz.RealRange}
+        @param z: Range in the z direction.
+        @type z: L{fuzz.RealRange}
         """
-        if p.x % self.pstep or p.y % self.pstep or p.z % self.pstep:
-            raise ValueError( "discrete point must fall on the grid" )
-        self.opaque.add( p )
+        for xi in arange( x[ 0 ], x[ 1 ], self.pstep ):
+            for yi in arange( y[ 0 ], y[ 1 ], self.pstep ):
+                for zi in arange( z[ 0 ], z[ 1 ], self.pstep ):
+                    self.opaque.add( Point( xi, yi, zi ) )
+                    for rho in arange( 0., pi + self.dstep, self.dstep ):
+                        for eta in arange( 0., 2 * pi, self.dstep ):
+                            try:
+                                self.D.remove( DirectionalPoint( \
+                                               xi, yi, zi, rho, eta ) )
+                            except KeyError:
+                                pass
+
+    def make_desired( self, x, y, z, d = None, mu = 1.0 ):
+        """\
+        Add a range to the desired coverage set.
+
+        @param x: Range in the x direction.
+        @type x: L{fuzz.RealRange}
+        @param y: Range in the y direction.
+        @type y: L{fuzz.RealRange}
+        @param z: Range in the z direction.
+        @type z: L{fuzz.RealRange}
+        @param d: Optional direction (will use all dstep otherwise).
+        @type d: C{tuple} of L{geometry.Angle}
+        @param mu: Membership value to assign to this range.
+        @type mu: C{float}
+        """
+        for dpoint in self._generate_points_specified( x, y, z, d ):
+            if not Point( dpoint.x, dpoint.y, dpoint.z ) in self.opaque:
+                self.D.add( dpoint, mu )
 
     def generate_points( self ):
         """\
-        Discrete directional point generator.
+        Discrete directional point generator for the entire scene.
 
         @return: The next directional point in the range.
         @rtype: L{geometry.DirectionalPoint}
         """
-        for x in arange( self.x[ 0 ], self.x[ 1 ], self.pstep ):
-            for y in arange( self.y[ 0 ], self.y[ 1 ], self.pstep ):
-                for z in arange( self.z[ 0 ], self.z[ 1 ], self.pstep ):
-                    if Point( x, y, z ) in self.opaque:
+        return self._generate_points_specified( self.x, self.y, self.z )
+
+    def _generate_points_specified( self, x, y, z, d = None ):
+        """\
+        General discrete directional point generator.
+
+        @param x: Range in the x direction.
+        @type x: L{fuzz.RealRange}
+        @param y: Range in the y direction.
+        @type y: L{fuzz.RealRange}
+        @param z: Range in the z direction.
+        @type z: L{fuzz.RealRange}
+        @param d: Optional direction (will use all dstep otherwise).
+        @type d: C{tuple} of L{geometry.Angle}
+        @return: The next directional point in the range.
+        @rtype: L{geometry.DirectionalPoint}
+        """
+        for xi in arange( x[ 0 ], x[ 1 ], self.pstep ):
+            for yi in arange( y[ 0 ], y[ 1 ], self.pstep ):
+                for zi in arange( z[ 0 ], z[ 1 ], self.pstep ):
+                    if Point( xi, yi, zi ) in self.opaque:
                         continue
+                    if d is not None:
+                        yield DirectionalPoint( xi, yi, zi, d[ 0 ], d[ 1 ] )
                     for rho in arange( 0., pi + self.dstep, self.dstep ):
                         if rho in [ 0, pi ]:
-                            yield DirectionalPoint( x, y, z, rho, 0 )
+                            yield DirectionalPoint( xi, yi, zi, rho, 0 )
                             continue
                         for eta in arange( 0., 2 * pi, self.dstep ):
-                            yield DirectionalPoint( x, y, z, rho, eta )
+                            yield DirectionalPoint( xi, yi, zi, rho, eta )
 
     def occluded( self, p, cam = Point( 0, 0, 0 ) ):
         """\
@@ -248,8 +299,10 @@ class Camera( object ):
         ahr = 2. * atan( ( ( w - ou ) * su ) / ( 2. * f ) )
         avl = 2. * atan( ( ov * sv ) / ( 2. * f ) )
         avr = 2. * atan( ( ( h - ov ) * sv ) / ( 2. * f ) )
-        gamma_h = ( float( gamma ) / float( w ) ) * 2.0 * sin( ( ahl + ahr ) / 2. )
-        gamma_v = ( float( gamma ) / float( h ) ) * 2.0 * sin( ( avl + avr ) / 2. )
+        gamma_h = ( float( gamma ) / float( w ) ) * 2.0 \
+                  * sin( ( ahl + ahr ) / 2. )
+        gamma_v = ( float( gamma ) / float( h ) ) * 2.0 \
+                  * sin( ( avl + avr ) / 2. )
         self.Cvh = TrapezoidalFuzzyNumber( \
                    ( -sin( ahl ) + gamma_h, sin( ahr ) - gamma_h ),
                    ( -sin( ahl ), sin( ahr ) ) )
@@ -406,6 +459,12 @@ class MultiCamera( IndexedSet ):
             if mu > 0 and not self.scene.occluded( dpoint, self[ key ].pose.T ):
                 dpoints.append( FuzzyElement( dpoint, mu ) )
         self.inscene[ key ] = FuzzySet( dpoints )
+
+    def performance( self ):
+        """\
+        Return the coverage performance of this multi-camera network.
+        """
+        return self.model.overlap( self.scene.D )
 
     def visualize( self, scale = 1.0, color = ( 1, 1, 1 ) ):
         """\
