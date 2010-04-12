eb5b56c0d6cad632dc47b1848e80f9b6d8b5c4a4
Aaron Mavrinac <mavrinac@gmail.com>
2010-04-11 22:56:10 -0400

    Single and dual experiment mega-commit. They're not going to work anyway.

--- exp_single.py
+++ exp_single.py
@@ -5,7 +5,7 @@ from numpy import arange
 from coverage import Scene, Camera, MultiCameraSimple, Point, visual_axes
 
 print "Creating camera model..."
-C = Camera( 'C', 4.3214, 12.1, 0.00465, 0.00465, 680, 512, 1024, 1360, 784.2, 0, 3.0, 0.5, 0.036, 1.5 )
+C = Camera( 'C', 4.3214, 12.1, 0.00465, 0.00465, 680, 512, 1360, 1024, 784.2, 0, 3.0, 0.5, 0.036, 1.5 )
 print "Creating scene..."
 S = Scene( RealRange( ( -1000.0, 1000.0 ) ), RealRange( ( -1000.0, 1000.0 ) ), RealRange( ( 100.0, 2000.0 ) ), 100.0, pi / 2.0 )
 #for x in arange( -400, 500, 100 ):
