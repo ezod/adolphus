d70f92be1bcec3d1b8e0aee047b8075fb88df88c
Aaron Mavrinac <mavrinac@gmail.com>
2010-09-27 07:44:37 -0400

    Add SICK-IVP Ranger E/D camera model.

--- adolphus/models/cameras.py
+++ adolphus/models/cameras.py
@@ -8,6 +8,11 @@ class ProsilicaEC1350(VisualizationObject):
     def __init__(self, parent, frame=None):
         """\
         Constructor.
+
+        @param parent: Reference to the parent object.
+        @type parent: C{object}
+        @param frame: The parent frame (optional).
+        @type frame: L{visual.primitives.frame}
         """
         if not visual:
             raise VisualizationError("visual module not loaded")
@@ -29,4 +34,43 @@ class ProsilicaEC1350(VisualizationObject):
             material=visual.materials.plastic))
         self.add('clport', visual.cylinder(frame=self, pos=(-36.5, -14.5, 0),
             radius=4.5, axis=(-6, 0, 0), color=(0.8, 0.8, 0.8),
+            material=visual.materials.plastic))
+
+
+class SICKIVPRangerED(VisualizationObject):
+    """\
+    SICK-IVP Ranger E/D camera model.
+    """
+    def __init__(self, parent, frame=None):
+        """\
+        Constructor.
+
+        @param parent: Reference to the parent object.
+        @type parent: C{object}
+        @param frame: The parent frame (optional).
+        @type frame: L{visual.primitives.frame}
+        """
+        if not visual:
+            raise VisualizationError("visual module not loaded")
+        VisualizationObject.__init__(self, parent, frame)
+        self.add('body', visual.box(frame=self, length=106, height=49, width=37,
+            pos=(-44, 0, -6.5), color=(1, 1, 1),
+            material=visual.materials.plastic))
+        self.add('top', visual.box(frame=self, length=106, height=23, width=13,
+            pos=(-44, 0, 18.5), color=(1, 1, 1),
+            material=visual.materials.plastic))
+        self.add('bevell', visual.cylinder(frame=self, pos=(9, 11.5, 12),
+            radius=13, axis=(-106, 0, 0), color=(1, 1, 1),
+            material=visual.materials.plastic))
+        self.add('bevelr', visual.cylinder(frame=self, pos=(9, -11.5, 12),
+            radius=13, axis=(-106, 0, 0), color=(1, 1, 1),
+            material=visual.materials.plastic))
+        self.add('ecport', visual.cylinder(frame=self, pos=(-97, -13.5, 9.5),
+            radius=6, axis=(-15, 0, 0), color=(0.8, 0.8, 0.8),
+            material=visual.materials.plastic))
+        self.add('pwport', visual.cylinder(frame=self, pos=(-97, -13.5, -9),
+            radius=5, axis=(-12, 0, 0), color=(0.8, 0.8, 0.8),
+            material=visual.materials.plastic))
+        self.add('enport', visual.box(frame=self, length=2, height=18, width=22,
+            pos=(-98, 7, -3), color=(0.1, 0.1, 0.1),
             material=visual.materials.plastic))
