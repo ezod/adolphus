d623e36401459138cd29f302f50eb90bdd0accc1
Aaron Mavrinac <mavrinac@gmail.com>
2010-10-02 13:37:42 -0400

    Correct xyz to yzx for vision platform model.

--- adolphus/models/scene.py
+++ adolphus/models/scene.py
@@ -45,38 +45,38 @@ class VisionPlatform(object):
         if not visual:
             raise VisualizationError("visual module not loaded")
         self.vis = VisualizationObject(self)
-        for x in [20, 900]:
-            for y in [20, 1280, 2550]:
-                self.vis.add('bar%d%d' % (x, y), visual.box(frame=self.vis,
-                    pos=(x, y, 885), length=40, height=40, width=1770,
+        for y in [20, 900]:
+            for z in [20, 1280, 2550]:
+                self.vis.add('bar%d%d' % (y, z), visual.box(frame=self.vis,
+                    pos=(885, y, z), width=40, height=40, length=1770,
                     color=(1, 0.5, 0), opacity=opacity,
                     material=visual.materials.plastic))
-                if y == 1280:
+                if z == 1280:
                     continue
-                self.vis.add('wheel%d%d' % (x, y), visual.cylinder(frame=\
-                    self.vis, pos=(x - 12, y, -60), axis=(24, 0, 0), radius=50,
+                self.vis.add('wheel%d%d' % (y, z), visual.cylinder(frame=\
+                    self.vis, pos=(-60, y - 12, z), axis=(0, 24, 0), radius=50,
                     color=(0.1, 0.1, 0.1), opacity=opacity,
                     material=visual.materials.rough))
-        for x in [20, 900]:
-            for z in [20, 510, 1750]:
+        for x in [20, 510, 1750]:
+            for y in [20, 900]:
+                self.vis.add('bar%d%d' % (x, y), visual.box(frame=self.vis,
+                    pos=(x, y, 1285), length=40, height=40, width=2570,
+                    color=(1, 0.5, 0), opacity=opacity,
+                    material=visual.materials.plastic))
+        for x in [20, 510, 1750]:
+            for z in [20, 1280, 2550]:
                 self.vis.add('bar%d%d' % (x, z), visual.box(frame=self.vis,
-                    pos=(x, 1285, z), length=40, width=40, height=2570,
+                    pos=(x, 460, z), length=40, width=40, height=920,
                     color=(1, 0.5, 0), opacity=opacity,
                     material=visual.materials.plastic))
-        for y in [20, 1280, 2550]:
-            for z in [20, 510, 1750]:
-                self.vis.add('bar%d%d' % (y, z), visual.box(frame=self.vis,
-                    pos=(460, y, z), height=40, width=40, length=920,
-                    color=(1, 0.5, 0), opacity=opacity,
-                    material=visual.materials.plastic))
-        self.vis.add('top', visual.box(frame=self.vis, pos=(460, 1285, 1775),
-            width=10, height=2530, length=860, color=(0.1, 0.1, 0.1),
+        self.vis.add('top', visual.box(frame=self.vis, pos=(1775, 460, 1285),
+            length=10, width=2530, height=860, color=(0.1, 0.1, 0.1),
             opacity=opacity, material=visual.materials.plastic))
-        self.vis.add('steel', visual.box(frame=self.vis, pos=(460, 1590, 535),
-            width=10, height=1900, length=860, color=(0.6, 0.6, 0.6),
+        self.vis.add('steel', visual.box(frame=self.vis, pos=(535, 460, 1590),
+            length=10, width=1900, height=860, color=(0.6, 0.6, 0.6),
             opacity=opacity, material=visual.materials.rough))
-        self.vis.add('wood', visual.box(frame=self.vis, pos=(460, 650, 49.5),
-            width=19, height=1240, length=860, color=(0.91, 0.85, 0.64),
+        self.vis.add('wood', visual.box(frame=self.vis, pos=(49.5, 460, 650),
+            length=19, width=1240, height=860, color=(0.91, 0.85, 0.64),
             opacity=opacity, material=visual.materials.wood))
         axis, angle = self.pose.R.to_axis_angle()
         self.vis.transform(self.pose.T.tuple, axis, angle)
