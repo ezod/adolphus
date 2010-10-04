e64564d4210ab80b7da234863dbb0724560edaf4
Aaron Mavrinac <mavrinac@gmail.com>
2010-10-04 16:53:34 -0400

    Add vision platform model to fourset experiment.

--- experiments/fourset.py
+++ experiments/fourset.py
@@ -1,4 +1,4 @@
 from adolphus.models.cameras import ProsilicaEC1350
 from adolphus.models.lenses import ComputarM3Z1228CMP
 
-from adolphus.models.scene import CheckerCalibrationBoard
+from adolphus.models.scene import CheckerCalibrationBoard, VisionPlatform
