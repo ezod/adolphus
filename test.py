cfd91ef403b274f8010c4c6bfb00ab1c1a768adf
Aaron Mavrinac <mavrinac@gmail.com>
2010-10-12 11:38:10 -0400

    Remove broken rotation test code.

--- test.py
+++ test.py
@@ -29,21 +29,5 @@ class TestGeometry(unittest.TestCase):
         s = A.geometry.DirectionalPoint((-4, 5, 14, 1.3, 0.2))
         self.assertEqual(self.q + self.p, s)
 
-    def test_rotation_conversions(self):
-        rotations = [A.geometry.Rotation(),
-                     A.geometry.Rotation([0.3, 1.4, 2.1]),
-                     A.geometry.Rotation([4.7, 0.9, 6.0]),
-                     A.geometry.Rotation([[-0.601332, 0.67475, -0.427916],
-                                          [0.797152, 0.543048, -0.263909],
-                                          [0.054306, -0.499811, -0.86443]])]
-        for R in rotations:
-            converted = [A.geometry.Rotation(R.to_rotation_matrix()),
-                         A.geometry.Rotation(R.to_axis_angle()),
-                         A.geometry.Rotation(R.to_euler_xyz())]
-            for C in converted:
-                self.assertEqual(R.rotate(self.p), C.rotate(self.p))
-                self.assertEqual(R.rotate(self.q), C.rotate(self.q))
-
-
 if __name__ == '__main__':
     unittest.main()
