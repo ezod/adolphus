bcec7bc3fce7a5e9f969e7fc8527dc18ad5b0125
Aaron Mavrinac <mavrinac@gmail.com>
2011-01-30 13:31:17 -0500

    Remove all YAML files from version control.

--- experiments/fourset.py
+++
@@ -1,4 +0,0 @@
-from adolphus.models.cameras import ProsilicaEC1350
-from adolphus.models.lenses import ComputarM3Z1228CMP
-
-from adolphus.models.scene import CheckerCalibrationBoard, VisionPlatform
--- experiments/ocr.py
+++
@@ -1,5 +0,0 @@
-from adolphus.models.cameras import ProsilicaEC1350
-from adolphus.models.lenses import ComputarM3Z1228CMP
-from adolphus.models.robots import MitsubishiRV1A
-
-from adolphus.models.scene import VisionPlatform
