13cd235fc99f8dfecb6d2acd5c1462f97e20a746
Aaron Mavrinac <mavrinac@gmail.com>
2011-07-04 20:15:10 -0400

    Add HALCON calibration parameter parsing functionality.

---
+++ adolphus/halconparser.py
@@ -0,0 +1,68 @@
+"""\
+HALCON parameter parser module.
+
+@author: Aaron Mavrinac
+@organization: University of Windsor
+@contact: mavrin1@uwindsor.ca
+@license: GPL-3
+"""
+
+import pyximport; pyximport.install()
+
+from math import pi
+
+from .geometry import Pose, Point, Rotation
+
+
+def parse_internal(filename):
+    """\
+    Parse internal calibration parameters from a HALCON internal calibration
+    output file.
+
+    @param filename: The calibration output file to load from.
+    @type filename: C{str}
+    @return: Tuple containing the internal parameters f, s, o, and dim.
+    @rtype: C{tuple}
+    """
+    f, s, o, dim = None, [None, None], [None, None], [None, None]
+    with open(filename, 'r') as icfile:
+        for line in icfile:
+            line = line.rstrip()
+            if not line or line.startswith('#') or line.startswith('\t') \
+            or line.startswith('ParGroup'):
+                continue
+            value = float(line.split(':')[2][1:-1])
+            if line.startswith('Focus'):
+                f = 1000 * value
+            elif line.startswith('Sx'):
+                s[0] = 1000 * value
+            elif line.startswith('Sy'):
+                s[1] = 1000 * value
+            elif line.startswith('Cx'):
+                o[0] = value
+            elif line.startswith('Cy'):
+                o[1] = value
+            elif line.startswith('ImageWidth'):
+                dim[0] = int(value)
+            elif line.startswith('ImageHeight'):
+                dim[1] = int(value)
+    return f, s, o, dim
+
+
+def parse_external(filename):
+    """\
+    Parse external calibration parameters from a HALCON external calibration
+    output file.
+    
+    @param filename: The calibration output file to load from.
+    @type filename: C{str}
+    """
+    with open(filename, 'r') as ecfile:
+        for line in ecfile:
+            line = line.rstrip()
+            if line.startswith('t'):
+                T = Point([float(value) for value in line.split(' ')[1:]])
+            elif line.startswith('r'):
+                R = Rotation.from_euler('zyx', [(360.0 - float(value)) * 180.0 \
+                    / pi for value in line.split(' ')[1:]])
+    return Pose(T, R)
