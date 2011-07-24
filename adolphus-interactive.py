44207be9c814e3349893cb989562a7a143bc1ae3
Aaron Mavrinac <mavrinac@gmail.com>
2011-07-24 15:31:40 -0400

    Make main script comply with project code conventions.

--- adolphus-interactive.py
+++ adolphus-interactive.py
@@ -7,18 +7,18 @@ from adolphus import Experiment
 
 def main():
     parser = OptionParser()
-    parser.add_option("-c", "--conf", dest="conf", default=None, 
-        help="custom configuration file to load")
-    parser.add_option("-z", "--zoom", dest="zoom", default=False,
-        action="store_true", help="disable camera view and use visual zoom")
+    parser.add_option('-c', '--conf', dest='conf', default=None, 
+        help='custom configuration file to load')
+    parser.add_option('-z', '--zoom', dest='zoom', default=False,
+        action='store_true', help='disable camera view and use visual zoom')
 
-    (options, args) = parser.parse_args()
+    opts, args = parser.parse_args()
     try:
         model_file = args[0]
     except IndexError:
         model_file = None
 
-    experiment = Experiment(model_file, config_file=options.conf, zoom=options.zoom)
+    experiment = Experiment(model_file, config_file=opts.conf, zoom=opts.zoom)
     experiment.start()
     experiment.join()
 
